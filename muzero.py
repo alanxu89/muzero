8f442088c439f301cbd430d2f07a6c5f2fa02bf3
Xunnong Xu <xunnongxu@gmail.com>
2021-03-25 22:59:50 +0800

    complete muzero class

--- muzero.py
+++ muzero.py
@@ -142,3 +142,63 @@ class MuZero:
         self.reanalyse_worker = None
         self.replay_buffer_worker = None
         self.shared_storage_worker = None
+
+    def test(self, render=True, opponent=None, muzero_player=None, num_tests=1):
+        opponent = opponent if opponent else self.config.opponent
+        muzero_player = muzero_player if muzero_player else self.config.muzero_player
+        self_player_worker = SelfPlay(
+            self.checkpoint, self.Game, self.config, np.random.randint(10000))
+        results = []
+
+        for i in range(num_tests):
+            print(f"Testing {i+1}/{num_tests}")
+            results.append(self_player_worker.play_game(
+                0, 0, render, opponent, muzero_player))
+        self_player_worker.close_game()
+
+        if len(self.config.players) == 1:
+            result = np.mean([sum(history.reward_history)
+                              for history in results])
+        else:
+            result = np.mean([sum(reward for i, reward in enumerate(history.reward_history)
+                                  if history.to_play_history[i-1] == muzero_player)
+                              for history in results])
+
+    def load_model(self, checkpoint_path=None, replay_buffer_path=None):
+        if checkpoint_path:
+            if os.path.exists(checkpoint_path):
+                self.checkpoint = tf.keras.models.load_model(checkpoint_path)
+                print(f"\nUsing checkpoint from {checkpoint_path}")
+            else:
+                print(f"\nThere is no model saved in {checkpoint_path}.")
+
+        if replay_buffer_path:
+            if os.path.exists(replay_buffer_path):
+                with open(replay_buffer_path, "rb") as f:
+                    replay_buffer_infos = pickle.load(f)
+                self.replay_buffer = replay_buffer_infos["replay_buffer"]
+                self.checkpoint["num_played_steps"] = replay_buffer_infos[
+                    "num_played_steps"
+                ]
+                self.checkpoint["num_played_games"] = replay_buffer_infos[
+                    "num_played_games"
+                ]
+                self.checkpoint["num_reanalysed_games"] = replay_buffer_infos[
+                    "num_reanalysed_games"
+                ]
+
+                print(
+                    f"\nInitializing replay buffer with {replay_buffer_path}")
+            else:
+                print(
+                    f"Warning: Replay buffer path '{replay_buffer_path}' doesn't exist.  Using empty buffer."
+                )
+                self.checkpoint["training_step"] = 0
+                self.checkpoint["num_played_steps"] = 0
+                self.checkpoint["num_played_games"] = 0
+                self.checkpoint["num_reanalysed_games"] = 0
+
+
+if __name__ == "__main__":
+    muzero = MuZero()
+    muzero.train()
