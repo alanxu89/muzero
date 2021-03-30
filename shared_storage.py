d768b95ada12d3357f39c6fa9fcc9b8afdf4ad49
Xunnong Xu <xunnongxu@gmail.com>
2021-03-30 23:08:45 +0800

    get and set info methods

--- shared_storage.py
+++ shared_storage.py
@@ -20,6 +20,22 @@ class SharedStorage:
     def get_checkpoint(self):
         return copy.deepcopy(self.current_checkpoint)
 
+    def get_info(self, keys):
+        if isinstance(keys, str):
+            return self.current_checkpoint[keys]
+        elif isinstance(keys, list):
+            return {key: self.current_checkpoint[key] for key in keys}
+        else:
+            raise TypeError
+
+    def set_info(self, keys, values=None):
+        if isinstance(keys, str) and values is not None:
+            self.current_checkpoint[keys] = values
+        elif isinstance(keys, dict):
+            self.current_checkpoint.update(keys)
+        else:
+            raise TypeError
+
 
 if __name__ == "__main__":
     model = tf.keras.Sequential([
