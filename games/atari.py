03e3c1cf1a8c855c45aa942a2a63011e65036869
Xunnong Xu <xunnongxu@gmail.com>
2021-04-02 18:24:40 +0800

    resize and show img with cv2

--- games/atari.py
+++ games/atari.py
@@ -1,4 +1,5 @@
 import os
+import cv2
 
 from ale_py import ALEInterface
 
@@ -6,7 +7,7 @@ from games.abstract_game import AbstractGame
 
 
 class AtariGame(AbstractGame):
-    def __init__(self, rom_file=None, seed=None):
+    def __init__(self, rom_file=None, seed=None, img_size=(96, 96)):
         super().__init__()
         self.ale = ALEInterface()
         if seed is None:
@@ -15,6 +16,7 @@ class AtariGame(AbstractGame):
         if rom_file is None:
             rom_file = "/Users/alanquantum/MachineLearning/atari_roms/space_invaders.bin"
         self.ale.loadROM(rom_file)
+        self.img_size = img_size
 
     def legal_actions(self):
         return self.ale.getLegalActionSet()
@@ -22,16 +24,18 @@ class AtariGame(AbstractGame):
     def reset(self):
         self.ale.reset_game()
 
+        return cv2.resize(self.ale.getScreenRGB(), self.img_size)
+
     def step(self, action):
         reward = self.ale.act(action)
-        new_observation = self.ale.getScreenRGB()
+        new_observation = cv2.resize(self.ale.getScreenRGB(), self.img_size)
         game_over = self.ale.game_over()
 
         return new_observation, reward, game_over
 
     def render(self):
         # if cv2 is available, use cv2.imshow(self.ale.getScreenRGB())
-        return
+        return cv2.imshow("atari", self.ale.getScreenRGB())
 
     def close(self):
         return
@@ -40,5 +44,15 @@ class AtariGame(AbstractGame):
 if __name__ == "__main__":
     g = AtariGame(
         "/Users/alanquantum/MachineLearning/atari_roms/space_invaders.bin", 123)
-    screen_rgb = g.render()
-    print(screen_rgb.shape)
+    # screen_rgb = g.render()
+    img = g.reset()
+    print(img.shape)
+
+    # img = cv2.resize(img, (96, 96))
+
+    cv2.imshow("img", img)
+
+    cv2.waitKey(0)
+
+    # closing all open windows
+    cv2.destroyAllWindows()
